9cff98864f66f31ead5be7b4ed0b0519ec6d72e0
meticulousfan <elithofan@gmail.com>
2022-11-07 14:24:24 +0000

    first commit

---
+++ src/components/Experience/Experience.tsx
@@ -0,0 +1,405 @@
+import { Container } from "./styles";
+import ScrollAnimation from "react-animate-on-scroll";
+
+export function Experience() {
+  return (
+    <Container id="experience">
+      <h2>Work Experiences</h2>
+
+      <div className="projects">
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              {/* <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://github.com/magnetoasher"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={githubIcon} alt="GitHub" />
+                </a>
+              </div> */}
+            </header>
+            <div className="body">
+              <h3>Frontend Developer</h3>
+              <ul>
+                <li>
+                  Created successful websites that met requirements for
+                  objectives such as load speed and design.
+                </li>
+                <li>
+                  Supported continuous improvement by investigating alternatives
+                  and technologies for review.
+                </li>
+                <li>Brought mock-ups to life with HTML, CSS and JavaScript.</li>
+                <li>
+                  Designed software tools and subsystems to support software
+                  reuse and domain analysis.
+                </li>
+              </ul>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Node</li>
+                <li>React</li>
+                <li>Vue</li>
+                <li>Express</li>
+                <li>MySql</li>
+                <li>Ejs</li>
+                <li>GraphQL</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              {/* <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://github.com/joaotuliojt/github-search"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={githubIcon} alt="GitHub" />
+                </a>
+                <a
+                  href="https://github-search-blond.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLinkIcon} alt="Visitar site" />
+                </a>
+              </div> */}
+            </header>
+            <div className="body">
+              <h3>Full Stack Engineer</h3>
+              <ul>
+                <li>
+                  Monitored equipment functionality to verify optimal
+                  performance and adjust system controls
+                </li>
+                <li>
+                  Completed code debugging and implemented fixes to resolve
+                  problems based on priority system.
+                </li>
+                <li>
+                  Updated the Vue.JS made client-side into a React-based website
+                  with better user experience.
+                </li>
+                <li>
+                  Produced and maintained system requirements, designs and
+                  resource inventory documentation.
+                </li>
+                <li>
+                  Interpreted briefs to create high-quality coding that
+                  functions according to specifications.
+                </li>
+              </ul>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Redux</li>
+                <li>AWS</li>
+                <li>Vue</li>
+                <li>Typescript</li>
+                <li>Styled Components</li>
+                <li>GraphQL</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              {/* <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://joaotuliojt.github.io/sunnyside-agency/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLinkIcon} alt="Visitar site" />
+                </a>
+              </div> */}
+            </header>
+            <div className="body">
+              <h3>React Developer</h3>
+              <ul>
+                <li>
+                  Communicated complex computer information in terminology that
+                  non-technical individuals could easily understand
+                </li>
+                <li>
+                  Identified and communicated potential risks and roadblocks to
+                  properly execute projects
+                </li>
+                <li>
+                  Researched alternative solutions to problem spaces to decide
+                  best technical approaches
+                </li>
+                <li>
+                  Participated in code review meetings, providing input on bugs,
+                  inefficiencies and potential solutions to emergent issues
+                </li>
+                <li>
+                  Integrated innovative development techniques to shorten
+                  turnaround times and spur customer loyalty
+                </li>
+              </ul>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>NextJs</li>
+                <li>TypeScript</li>
+                <li>SSR</li>
+                <li>GraphQL</li>
+                <li>Vue/Quasar</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              {/* <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://joaotulio.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLinkIcon} alt="Visitar site" />
+                </a>
+              </div> */}
+            </header>
+            <div className="body">
+              <h3>Full Stack Developer</h3>
+              <ul>
+                <li>
+                  Converted native app to into React based website using
+                  Typescript and Next. languages
+                </li>
+                <li>
+                  Converted native app to into React based website using
+                  Typescript and Next.JS
+                </li>
+                <li>
+                  Installed front-end and back-end software applications and
+                  performed bug fixes.
+                </li>
+                <li>
+                  Assisted other developers with coding, troubleshooting and
+                  debugging.
+                </li>
+                <li>
+                  Built professional relationships with team and clients to help
+                  speed communication and attain project results
+                </li>
+              </ul>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>VueJs</li>
+                <li>NestJs</li>
+                <li>GraphQL</li>
+                <li>TypeORM</li>
+                <li>JavaScript</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        {/* <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://github.com/joaotuliojt/dtmoney-trilha-reactjs"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={githubIcon} alt="Visitar site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>NodeJs Backend Developer</h3>
+              <ul>
+                <li>
+                  Designed, implemented and integrated GraphQL APIs using
+                  Apollo, Nest.js, TypeORM, PostgreSQL
+                </li>
+                <li>Participated in code review and team design sessions</li>
+                <li>
+                  Contributed ideas and suggestions in team meetings and
+                  delivered updates on deadlines, designs and enhancements,
+                </li>
+                <li>
+                  Implemented work studio platform and OKR dashboard using
+                  React, TypeScript, AWS (lambda, API gateway, rest API, S3,
+                  Amplify), Node, MongoDB
+                </li>
+                <li>
+                  Monitored and tracked server bugs on AWS CloudWatch and fixed
+                  them
+                </li>
+                <li>Used testing frameworks like Jest/Mocha to test APIs</li>
+                <li>Performed migration from MySQL to MongoDB</li>
+                <li>
+                  Worked on creating different type of indexes based on
+                  different collections to get good performance in MongoDB
+                </li>
+                <li>
+                  Containerized all the Ticketing related applications using
+                  Docker
+                </li>
+                <li>
+                  Managed Container using Docker by writing Dockerfiles and set
+                  up the automated build on Docker HUB and installed and
+                  configured kubernets Used infrastructure tools like Terraform
+                  and AWS
+                </li>
+              </ul>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Vue</li>
+                <li>NodeJs</li>
+                <li>Express</li>
+                <li>Jest/Mocha</li>
+                <li>MySQL/MongoDB</li>
+                <li>Docker</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation> */}
+
+        {/* <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://github.com/joaotuliojt/dv-musica-library-page"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={githubIcon} alt="Visitar site" />
+                </a>
+                <a
+                  href="https://joaotuliojt.github.io/dv-musica-library-page/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLinkIcon} alt="Visitar site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Music Library</h3>
+              <p>
+                Uma interface para uma biblioteca de músicas, semelhante ao
+                Spotify.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Html</li>
+                <li>Css</li>
+                <li>JavaSript</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation> */}
+      </div>
+    </Container>
+  );
+}
